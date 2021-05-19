55dd564f06561b1ae1533b5b127f34fc8b889d28
Vladimir P <enrichment.off@gmail.com>
2021-05-19 17:51:24 +0300

    lesson/3369/step/7

---
+++ cycle/lesson3369_7.py
@@ -0,0 +1,18 @@
+﻿'''
+    https://stepik.org/lesson/3369/step/7?unit=952
+'''
+
+summ = 0
+result = 0
+list = []
+while True:
+    temp = int(input())
+    summ += temp
+    list.append(temp)
+    if summ == 0:
+        for i in list:
+            result += i * i
+        break
+
+
+print(result)
