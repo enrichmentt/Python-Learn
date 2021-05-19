128d9b6114be89fcac9a6b8de4b0b625a509f8d3
Vladimir P <enrichment.off@gmail.com>
2021-05-19 16:19:52 +0300

    lesson/3368/step/11

---
+++ lists/lesson3368_10.py
@@ -0,0 +1,28 @@
+﻿'''
+    https://stepik.org/lesson/3368/step/10?unit=951
+
+    Sample Input 1:
+
+    1 3 5 6 10
+    Sample Output 1:
+
+    13 6 9 15 7
+'''
+
+list = [int(i) for i in input().split()]
+res = ""
+index_middle = 0
+
+while index_middle < len(list):
+    if len(list) == 1:
+        print(list[0])
+        break
+
+    index_first = index_middle - 1
+    index_second = index_middle + 1
+
+    if index_middle == len(list) - 1:
+        index_second = 0
+
+    print(list[index_first] + list[index_second], end=" ")
+    index_middle += 1
