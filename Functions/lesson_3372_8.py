3b02aed36f75b2e919650b584395bb19516ffe63
Vladimir P <enrichment.off@gmail.com>
2021-05-30 22:01:05 +0300

    решение задачи

---
+++ Functions/lesson_3372_8.py
@@ -0,0 +1,15 @@
+﻿'''
+    https://stepik.org/lesson/3372/step/8?unit=955
+'''
+
+
+def f(x):
+    if x <= -2:
+        return 1 - (x + 2) ** 2
+    elif x > -2 and x <= 2:
+        return x * -1 / 2
+    elif x > 2:
+        return (x - 2) ** 2 + 1
+
+
+print(f(4.5))  # 7.25
