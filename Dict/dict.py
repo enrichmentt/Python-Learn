00c1f52f2a07bf290814da7983a04664c1469abc
Vladimir P <enrichment.off@gmail.com>
2021-05-30 22:59:09 +0300

    перебор словаря

---
+++ Dict/dict.py
@@ -0,0 +1,13 @@
+﻿d = {'A': 12, 'B': 1, 'D': 19, 'E': 22}
+
+for key in d:
+    print(key, end=' ')
+print()
+for key in d.keys():
+    print(key, end=' ')
+print()
+for value in d.values():
+    print(value, end=' ')
+print()
+for key, item in d.items():
+    print(key, "-", item, end=' ')
