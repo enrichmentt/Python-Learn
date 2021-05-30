9bf56dc1eb026f66bb9662841f5b4d5a895135dd
Vladimir Pekshev <enrichment.off@gmail.com>
2021-05-30 15:11:29 +0300

    Add def min

---
+++ Functions/minimum.py
@@ -0,0 +1,10 @@
+﻿# *numbers - произвольное количество аргументов
+def min(*numbers):
+    min = numbers[0]
+    for number in numbers:
+        if min > number:
+            min = number
+    return min
+
+
+print(min(2, 3, 1, 4, 5, 6))
